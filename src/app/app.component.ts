a83fe6416adb92b6d59d0e5ed1477a6dd9454449
alexiagabrielly <alexiagabrielly@ibm.com>
2019-12-26 14:44:39 -0300

    criar a interface que define os tipos do post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { Post } from './posts/post.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  storedPosts = [];
+  storedPosts: Post[] = [];
 
   // Método responsável por armazenar os dados do post que vem do componente post-create
   onPostAdded(post) {
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
+import { Post } from '../post.model';
+
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -11,10 +13,10 @@ export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
   // Output responsável por fazer o angular entender que o postCreated é um evento que pode ser escutado do lado de fora
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
 
   onAddPost() {
-    const post = {
+    const post: Post = {
       title: this.enteredTitle,
       content: this.enteredContent
     };
