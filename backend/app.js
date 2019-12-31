1d1d1953341487ddaafe74b71a80c6f5fc2f7ad6
alexiagabrielly <alexiagabrielly@ibm.com>
2019-12-31 10:26:00 -0300

    Fazer endpoint de atualizar post e reutilizar o mesmo componente de criar post para isso

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, DELETE, OPTIONS, PUT"
   );
   next();
 });
@@ -45,6 +45,27 @@ app.post("/api/posts", (req, res, next) => {
       message: 'Post added successfully',
       postId: createdPost._id
     });
+  });
+});
+
+app.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({_id: req.params.id}, post).then(result => {
+    res.status(200).json({message: "Update successful!"});
+  });
+});
+
+app.get("/api/posts/:id", (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Post not found!" });
+    }
   });
 });
 
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
 import { PostsService } from '../posts.service';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
@@ -10,15 +12,48 @@ import { PostsService } from '../posts.service';
 })
 export class PostCreateComponent implements OnInit {
 
-  constructor(public postService: PostsService) {}
+  post: Post;
+  isLoading = false;
+  private mode = "create";
+  private postId: string;
 
-  onAddPost(form: NgForm) {
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
+
+  onSavePost(form: NgForm) {
     if (form.invalid) return;
-    this.postService.addPost(form.value.title, form.value.content);
+    if (this.mode === "create") {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(
+        this.postId,
+        form.value.title,
+        form.value.content
+      );
+    }
     form.resetForm();
   }
-
+  
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has("postId")) {
+        this.mode = "edit";
+        this.postId = paramMap.get("postId");
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content
+          };
+        });
+      } else {
+        this.mode = "create";
+        this.postId = null;
+      }
+    });
   }
+
 
 }
