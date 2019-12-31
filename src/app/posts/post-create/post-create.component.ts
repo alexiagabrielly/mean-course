0e30b9492dedc977e3d981816c6b07f169a3772d
alexiagabrielly <alexiagabrielly@ibm.com>
2019-12-31 14:37:27 -0300

    criar endpoint de upload de arquivo e integrar com o frontend

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -26,10 +26,14 @@ export class PostCreateComponent implements OnInit {
   ) {}
 
   onSavePost() {
-    if (this.form.invalid) return;
+    // if (this.form.invalid) return;
     this.isLoading = true;
     if (this.mode === "create") {
-      this.postsService.addPost(this.form.value.title, this.form.value.content);
+      this.postsService.addPost(
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image
+      );
     } else {
       this.postsService.updatePost(
         this.postId,
@@ -46,7 +50,7 @@ export class PostCreateComponent implements OnInit {
     this.form.get('image').updateValueAndValidity();
     const reader = new FileReader();
     reader.onload = () => {
-      this.imagePreview = reader.result;
+      this.imagePreview = reader.result as string;
     };
     reader.readAsDataURL(file);
   }
