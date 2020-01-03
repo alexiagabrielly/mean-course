6d2b99458af118d19edef3982960aec8f15eb266
alexiagabrielly <alexiagabrielly@ibm.com>
2020-01-03 10:10:24 -0300

    Protejer rotas que só podem ser acessadas por usuarios autenticados e fazer com que o usuarios seja desconectado depois de uma hora

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 export class AuthService {
   private isAuthenticated = false;
   private token: string;
+  private tokenTimer: any;
   // Subject responsável por empurrar as informações de autenticação para os componentes interessados
   private authStatusListener = new Subject<boolean>();
 
@@ -41,11 +42,15 @@ export class AuthService {
         email: email,
         password: password
     };
-      this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
+      this.http.post<{token: string, expiresIn: number}>("http://localhost:3000/api/user/login", authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
         if(token) {
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() => {
+            this.logout();
+          }, expiresInDuration * 1000);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           this.router.navigate(['/']);
@@ -58,5 +63,6 @@ export class AuthService {
       this.isAuthenticated = false;
       this.authStatusListener.next(false);
       this.router.navigate(['/']);
+      clearTimeout(this.tokenTimer);
     }
 }
